f3a4d49e06cded4cb249810d5403d32d997f76da
Rafael <rafabb1350@gmail.com>
2020-11-17 12:36:45 -0300

    Criação do DAO Generic

--- hospital/src/main/java/br/com/model/PessoaBean.java
+++ hospital/src/main/java/br/com/model/PessoaBean.java
@@ -1,4 +1,4 @@
-package br.com.hospital;
+package br.com.model;
 
 import javax.faces.bean.ManagedBean;
 import javax.persistence.Entity;
@@ -15,7 +15,35 @@ public class PessoaBean {
 	private Long id;	
 	private String nome;
 	private String sobrenome;
+	private String login;
+	private String senha;
+	private String nivel;
+
 	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getLogin() {
+		return login;
+	}
+	public void setLogin(String login) {
+		this.login = login;
+	}
+	public String getSenha() {
+		return senha;
+	}
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+	public String getNivel() {
+		return nivel;
+	}
+	public void setNivel(String nivel) {
+		this.nivel = nivel;
+	}
 	public String getNome() {
 		return nome;
 	}
--- hospital/src/test/java/br/com/hospital/TesteHibernate.java
+++ hospital/src/test/java/br/com/hospital/TesteHibernate.java
@@ -2,13 +2,25 @@ package br.com.hospital;
 
 import org.junit.Test;
 
-import jpautil.JPAUtil;
+import br.com.dao.DaoGeneric;
+import br.com.jpautil.JPAUtil;
+import br.com.model.PessoaBean;
 
 public class TesteHibernate {
 	
 	@Test
 	public void testeHibernate() {
-		JPAUtil.getEntityManager();
+		DaoGeneric<PessoaBean> daoGeneric = new DaoGeneric<PessoaBean>();
+		
+		PessoaBean pessoa = new PessoaBean();
+		
+		pessoa.setLogin("teste");
+		pessoa.setNome("Rafael");
+		pessoa.setSobrenome("Lima");
+		pessoa.setSenha("teste");
+		pessoa.setNivel("ADMINISTRATIVO");
+		
+		daoGeneric.salvar(pessoa);
 	}
 
 }
